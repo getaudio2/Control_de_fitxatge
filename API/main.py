6d18a30538c6457da73ef827b8f129eea5e4a02d
Miguel Vera <2023_miguel.vera@iticbcn.cat>
2024-11-22 18:06:29 +0100

    API modificado

--- API/main.py
+++ API/main.py
@@ -81,6 +81,15 @@ def list_personas():
     conn.close()
     return personas
 
+@app.get("/persona/alumnosAll")
+def list_personas():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM persona where rol = 'Alumno'")
+    personas = cursor.fetchall()
+    conn.close()
+    return personas
+
 @app.get("/asistencia/listAll")
 def list_asiste():
     conn = get_db_connection()
--- README.md
+++ README.md
@@ -10,10 +10,18 @@ SELECT * FROM persona
 ![alt text](image.png)
 
 ````
-SELECT * FROM persona where rol = "profesor";
+SELECT * FROM persona where rol = 'Profesor';
 ````
 ![alt text](image-2.png)
 
+````
+SELECT * FROM persona where rol = 'Alumno';
+````
+![alt text](image-3.png)
+
 # Consulta Asistencia
 
+````
+"SELECT * FROM asistencia";
+````
 ![alt text](image-1.png)

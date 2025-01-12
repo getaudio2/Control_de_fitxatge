637dae7af0d4f50fc95abf22e62de0ea6650a6ab
Miguel Vera <2023_miguel.vera@iticbcn.cat>
2025-01-12 23:46:37 +0100

    local

--- API/client.py
+++ API/client.py
@@ -4,8 +4,8 @@ def get_db_connection():
     """Returns a connection to the database."""
     connection = mysql.connector.connect(
         host="127.0.0.1",
-        user="paul",
-        password="admin",
+        user="miguel",
+        password="3008",
         database="control_fitxatge",
         charset='utf8mb4',
         collation='utf8mb4_general_ci',
